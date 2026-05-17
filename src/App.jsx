ff109fe1cde0be8171e4588d64d4b0141446a57a
LucasDimon <lucas_dimon@estudante.sesisenai.org.br>
2026-05-17 20:19:05 -0300

    feat: cria estrutura de pastas e rotas principais

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Comparacao from './pages/Comparacao';
+import Historico from './pages/Historico';
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* Menu de Navegação Provisório */}
+      <nav style={{ padding: '10px', background: '#eee', marginBottom: '20px' }}>
+        <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+        <Link to="/login" style={{ marginRight: '10px' }}>Login</Link>
+        <Link to="/historico" style={{ marginRight: '10px' }}>Histórico</Link>
+      </nav>
+
+      {/* Configuração das Rotas */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/comparacao" element={<Comparacao />} />
+        <Route path="/historico" element={<Historico />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Historico.jsx
@@ -0,0 +1,3 @@
+export default function Historico() {
+  return <h1>Histórico de Comparações</h1>;
+}
